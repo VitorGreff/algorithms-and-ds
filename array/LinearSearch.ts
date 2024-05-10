9579ee24d3a450c35f4d8fc4f268ef490caf48b3
VitorGreff <vitorgreffdias@gmail.com>
2024-05-10 00:58:10 -0300

    fixing folder conflicts

--- array/LinearSearch.ts
+++
@@ -1,6 +0,0 @@
-export default function LinearSearch(haystack: number[], needle: number): boolean {
-    for (let i = 0; i < haystack.length; i++) {
-        if (haystack[i] === needle) return true
-    }
-    return false
-}
